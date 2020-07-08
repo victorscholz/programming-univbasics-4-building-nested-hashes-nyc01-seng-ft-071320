d8a5a087b2ccb4106e0631e8e92a875b7c7d9904
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-08 05:09:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,13 +3,10 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  monopoly = {
-    :railroads => 
-    { 
+  monopoly = {:railroads => { 
       :pieces => 4, 
       :rent_in_dollars => 
       {
-        
       },
       :names => {
       }
